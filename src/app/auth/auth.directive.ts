13006ce09ebf45b74855351aed32e7b1939c209a
Lucas Racoci <racoci.0@gmail.com>
2025-02-26 10:32:06 -0300

    Implement auth directive

--- src/app/auth/auth.directive.ts
+++ src/app/auth/auth.directive.ts
@@ -1,11 +1,24 @@
-import { Directive } from '@angular/core';
+import {Directive, effect, inject, input, TemplateRef, ViewContainerRef} from '@angular/core';
+import {AuthService} from "./auth.service";
+import {Permission} from "./auth.model";
 
 @Directive({
   selector: '[appAuth]',
   standalone: true
 })
 export class AuthDirective {
+  userType = input.required<Permission>({alias: 'appAuth'})
+  private authService = inject(AuthService);
+  private template = inject(TemplateRef);
+  private viewContainer =  inject(ViewContainerRef);
+  constructor() {
+    effect(() => {
+      if (this.authService.activePermission() === this.userType()) {
+        this.viewContainer.createEmbeddedView(this.template);
+      } else {
+        this.viewContainer.clear()
+      }
+    });
 
-  constructor() { }
-
+  }
 }
